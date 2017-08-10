7cfd0e93b2b6cf5964dabc1c559e6f66c0eec87d
morgan Peck <mlpeck@fullsail.edu>
2017-08-10 14:08:50 -0500

    tryiing to get my navigation to work so when the window size is different.

--- apiData.js
+++ apiData.js
@@ -7,32 +7,31 @@ var userInput;
 var newURL;
 var search=document.getElementById('searchButton');
  var movieResults; 
-function searchForMovies(){   
+function searchForMovies()
+{   
     userInput=document.querySelector('#movie').value; 
     newURL=url+userInput+"&page=1&include_adult=false";
 //Loading the json data from The Movie Database
     fetch(newURL)
     .then(response=>response.json())
-    .then(responseAsJson=>{
+    .then(responseAsJson=>
+    {
 //Variables to hold results
-   
+     
     movieResults+='<h1 id="resultsHeader">Results for '+document.querySelector('#movie').value+'</h1>';   
-    movieResults+='<section id="movies">';  
-    //Looping through the api data returned
+    movieResults+='<section id="movies">';
+     //Looping through the api data returned
     for(var i=0; i<responseAsJson.results.length;i++)
         {
             //conditional to limit reults to only 9 
-            if(i<9){
-               
+            if(i<9)
+            {
                 movieResults+='<article id="movieSearchResults">';
                 movieResults+='<img src="https://image.tmdb.org/t/p/w500'+responseAsJson.results[i]["poster_path"]+'" id="moviePoster" />';
                 movieResults+='<span id="data"><h5 id="movieTitle">'+responseAsJson.results[i]["title"]+'</h5>';
                 movieResults+='<h6 id="movieYear">'+responseAsJson.results[i]["release_date"]+'</h6></span>';
                 movieResults+='</article>';
-           
-            }
-              
-            
+            }         
         }
   movieResults+='</section>';
   var searchResults=document.getElementById('results');
@@ -40,11 +39,11 @@ function searchForMovies(){
     {
         document.getElementById('results').innerHTML=movieResults;
     }
-})
+    })
     .catch(Error=>{
     console.log('error',Error);
     })
-   }
+}
 //
 search.addEventListener('click',searchForMovies,false);
 
